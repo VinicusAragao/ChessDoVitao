c58ad4dedf93f15f39cc338e55975bffbcda6c07
Vinicius Sviatovski Aragão <116413989+VinicusAragao@users.noreply.github.com>
2022-12-03 21:55:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const main = document.getElementsByTagName('main')[0]
 const playerText = document.getElementsByTagName('h2')[0]
-let tileColor = 'black'
-let lineStart = 'black'
+let tileColor = 'white'
+let lineStart = 'white'
 let boardMap = []
 let grabing = false
-let grabedObject
+let grabedTile
+let grabedPiece
 let playerOrder = 'white'
+let playerOpposite = 'black'
 
 class tile{
   constructor(element){
@@ -13,21 +15,28 @@ class tile{
     this.row = parseInt(element.dataset.object[0])
     this.cell = parseInt(element.dataset.object[1])
     this.tileColor = element.classList[1]
-    this.piece = element.getElementsByClassName('piece')
     this.containingPiece = 'none'
-    this.player = 'none'
+  }
+}
+class piece{
+  constructor(row,cell,piece,player,element){
+    this.htmlElement = element
+    this.position = [row,cell]
+    this.piece = piece
+    this.player = player
     this.alreadyPlayed = false
     this.possibleMoves = []
+    this.possibleMovesCondition = []
   }
   checkPossibleMoves(){
     console.log('checando possibilidades...')
     const player = this.player
-    const row = this.row
-    const cell = this.cell
-    const played = this.alreadyPlayed
+    const row = parseInt(this.position[0])
+    const cell = parseInt(this.position[1])
+    const alreadyPlayed = this.alreadyPlayed
+    const piece = this.piece
     let object
     let colorDiference
-
     if(player === 'white'){
       colorDiference = -1
     }
@@ -35,36 +44,35 @@ class tile{
       colorDiference = 1
     }
 
-    switch(this.containingPiece){
+    switch(piece){
     case 'pawn':
-      checkUndefined(row+(1*colorDiference),cell)
-      if(!played){
-        checkUndefined(row+(2*colorDiference),cell)
-      }
-      checkUndefined(row+(1*colorDiference),cell-1)
-      checkUndefined(row+(1*colorDiference),cell+1)
+      passMove(row+(1*colorDiference),cell,'normal')
+      passMove(row+(2*colorDiference),cell,'neverPlayed')
+      passMove(row+(1*colorDiference),cell-1,'attack')
+      passMove(row+(1*colorDiference),cell+1,'attack')
     break
     case 'rook':
-      for(let i = 1; i < 9;i++){
-        checkUndefined(row-i,cell)
-        checkUndefined(row+i,cell)
-        checkUndefined(row,cell-i)
-        checkUndefined(row,cell+i)
-      }
+        checkMoveWithCollision(row,cell,1,0)//up
+        checkMoveWithCollision(row,cell,0,1)//right
+        checkMoveWithCollision(row,cell,-1,0)//down
+        checkMoveWithCollision(row,cell,0,-1)//left
     break
     case 'knight':
-        checkUndefined(row+2,cell+1)
-        checkUndefined(row+2,cell-1)
-        checkUndefined(row-2,cell+1)
-        checkUndefined(row-2,cell-1)
+        passMove(row+2,cell+1,'normal')
+        passMove(row+2,cell-1,'normal')
+        passMove(row-2,cell+1,'normal')
+        passMove(row-2,cell-1,'normal')
 
-        checkUndefined(row+1,cell+2)
-        checkUndefined(row+1,cell-2)
-        checkUndefined(row-1,cell+2)
-        checkUndefined(row-1,cell-2) 
+        passMove(row+1,cell+2,'normal')
+        passMove(row+1,cell-2,'normal')
+        passMove(row-1,cell+2,'normal')
+        passMove(row-1,cell-2,'normal') 
     break
     case 'bishop':
-
+        checkMoveWithCollision(row,cell,1,-1)//topLeft
+        checkMoveWithCollision(row,cell,1,1)//topRight
+        checkMoveWithCollision(row,cell,-1,-1)//bottomLeft
+        checkMoveWithCollision(row,cell,-1,+1)//bottomRight
     break
     case 'king':
 
@@ -74,31 +82,69 @@ class tile{
     }
   }
 }
-// class piece{
-//   constructor(){
-    
-//   }
-// }
-function checkUndefined(row,cell){
+function isUndefined(row,cell){
   if(row > 7 || row < 0 || cell > 7 || cell < 0){
-    return
+    return true
   }
   else{
-    checkMove(grabedObject,boardMap[row][cell])
+    return false
   }
 }
-function checkMove(holdedObject,newObject){
-  const color = holdedObject.player
-  let otherPlayer
-
-  if(color === 'black'){otherPlayer = 'white'}
-  else if(color === 'white'){otherPlayer = 'black'}
-  if(newObject.containingPiece === 'none' || newObject.player === otherPlayer){
-    holdedObject.possibleMoves.push(newObject)
+function checkMoveWithCollision(initialRow,initialCell,rowIncrement,cellIncrement){
+  let row = initialRow + rowIncrement
+  let cell = initialCell + cellIncrement
+  for(let i = 0; i < 9; i++){
+    console.log(i)
+    if(isUndefined(row,cell)){
+      console.log(undefined)
+      return
+    }
+    const object = boardMap[row][cell]
+    if(object.containingPiece.player === playerOrder){
+      console.log('amigo')
+      break
+    }
+    else if(object.containingPiece.player === playerOpposite){
+      console.log('inimigo')
+      grabedPiece.possibleMoves.push(object)
+      grabedPiece.possibleMovesCondition.push('collision')
+      break
+    }
+    grabedPiece.possibleMoves.push(object)
+    grabedPiece.possibleMovesCondition.push('collision')
+    row += rowIncrement
+    cell += cellIncrement
+    console.log('nada')
+    console.log(row +' '+cell)
   }
-  console.log(holdedObject.possibleMoves)
+  console.log(grabedPiece.possibleMoves)
 }
-
+function passMove(row,cell,condition){
+  if(isUndefined(row,cell)){
+    return
+  }
+  const newTile = boardMap[row][cell]
+  if(condition === 'attack'){
+    if(newTile.containingPiece.player !== playerOpposite){
+      return
+    }
+  }
+  else if(condition === 'neverPlayed'){
+    if(grabedPiece.alreadyPlayed === true){
+      return
+    }
+  }
+  else if(condition === 'normal'){
+    if(newTile.containingPiece.player === playerOrder){
+      return
+    }
+  }
+  else if(condition === 'collision'){
+    return
+  }
+  grabedPiece.possibleMoves.push(newTile)
+  grabedPiece.possibleMovesCondition.push(condition)
+}
 window.addEventListener('load',createBoard)
 function createBoard(){
   for(let r = 1; r <= 8; r++){
@@ -186,27 +232,37 @@ function placePieces(){
     createPiece(6,cell,'pawn','white')
   }
 }
-function createPiece(row,coll,piece,color){
-  const object = boardMap[row][coll]
+function createPiece(row,cell,pieceType,color){
   const el = document.createElement('img')
-        el.classList.add('piece',piece)
-        el.setAttribute('src', `images/${color+capitalize(piece)}.png`)
+        el.classList.add('piece',pieceType)
+        el.setAttribute('src', `images/${color+capitalize(pieceType)}.png`)
         el.setAttribute('draggable', false)
-  object.htmlElement.appendChild(el)
-  object.htmlElement.addEventListener('pointerdown', holdPiece)
-  object.containingPiece = piece
-  object.player = color
+
+  const pieceObject = new piece(row,cell,pieceType,color,el)        
+  const tileObject = boardMap[row][cell]
+  tileObject.htmlElement.appendChild(el)
+  tileObject.htmlElement.addEventListener('pointerdown', holdPiece)
+  tileObject.containingPiece = pieceObject
+  return el
 }
 function capitalize(string){
   return string[0].toUpperCase() + string.slice(1)
 }
-function holdPiece(){
-  if(!grabing){
+function holdPiece(e,optionalElement){
+  console.log('a')
+  newElement = this
+  if(optionalElement !== undefined){
+    newElement = optionalElement
+  }
+  const path = newElement.dataset.object
+  const newTile =  boardMap[path[0]][path[1]]
+  console.log(newTile)
+  if(!grabing || newTile.containingPiece.player === grabedPiece.player){
     console.log('pegou')
-    const path = this.dataset.object
-    grabedObject = boardMap[path[0]][path[1]]
-    if(grabedObject.player === playerOrder){
-      grabedObject.checkPossibleMoves()
+    grabedTile = boardMap[path[0]][path[1]]
+    grabedPiece = boardMap[path[0]][path[1]].containingPiece
+    if(grabedPiece.player === playerOrder){
+      grabedPiece.checkPossibleMoves()
       const tiles = document.getElementsByClassName('cell')
       for(let i = 0; i < tiles.length;i++){
         tiles[i].addEventListener('pointerdown',newPieceTile)
@@ -217,55 +273,80 @@ function holdPiece(){
 }
 function newPieceTile(){
   if(grabing){
-    const path = this.dataset.object
-    const newTile = boardMap[path[0]][path[1]]
-
-    if(newTile.player !== grabedObject.player){
-      for(let i = 0; i < grabedObject.possibleMoves.length; i++){
-        if(JSON.stringify(newTile) === JSON.stringify(grabedObject.possibleMoves[i])){
-          createPiece(path[0],path[1],grabedObject.containingPiece,grabedObject.player)
-          setSizes()
-          newTile.htmlElement.addEventListener('pointerdown', holdPiece)
-
-          grabedObject.containingPiece = 'none'
-          grabedObject.player = 'none'
-          grabedObject.possibleMoves = []
-          grabedObject.alreadyPlayed = true
-          grabedObject.htmlElement.removeEventListener('pointerdown', holdPiece)
-          grabedObject.htmlElement.getElementsByClassName('piece')[0].remove()
-
-          const tiles = document.getElementsByClassName('cell')
-          for(let i = 0; i < tiles.length;i++){
-            tiles[i].removeEventListener('pointerdown',newPieceTile)
+    const newPath = this.dataset.object
+    const newTile = boardMap[newPath[0]][newPath[1]]
+    console.log('hi')
+    if(newTile.containingPiece.player !== grabedPiece.player){
+      for(let i = 0; i < grabedPiece.possibleMoves.length; i++){
+        console.log(grabedPiece.possibleMoves)
+        if(JSON.stringify(newTile) === JSON.stringify(grabedPiece.possibleMoves[i])){
+          const condition = grabedPiece.possibleMovesCondition[i]
+          if(condition === 'attack' && newTile.containingPiece.player === playerOpposite){
+            movePiece(newPath,newTile)
           }
-          if(playerOrder === 'white'){
-            playerOrder = 'black'
-            playerText.textContent = 'Preto'
-            playerText.style.color = 'black'
-          }else if(playerOrder === 'black'){
-            playerOrder = 'white'
-            playerText.textContent = 'Branco'
-            playerText.style.color = 'white'
+          else if(condition === 'neverPlayed' && grabedPiece.alreadyPlayed === false){
+            movePiece(newPath,newTile)
           }
-          grabing = false
+          else if(condition === 'normal' || condition === 'collision'){
+            movePiece(newPath,newTile)
+          }
+          else{
+            return
+          }
         }
       }
     }
+    else if(newTile.containingPiece.player === grabedPiece.player){
+      console.log('same?')
+      grabedPiece.possibleMoves = []
+      grabedPiece.possibleMovesCondition = []
+      holdPiece('none',newTile.htmlElement)
+    }
   }
 }
+function movePiece(newPath,newTile){
+  newTile.containingPiece.htmlElement?.remove()
+  const newPieceElement = createPiece(newPath[0],newPath[1],grabedPiece.piece,grabedPiece.player)
+  setSizes()
+  grabedPiece.position = [newPath[0],newPath[1]]
+  grabedPiece.alreadyPlayed = true
+  grabedPiece.possibleMoves = []
+  grabedPiece.possibleMovesCondition = []
+  grabedPiece.htmlElement.remove()
+  grabedPiece.htmlElement = newPieceElement
 
+  grabedTile.htmlElement.removeEventListener('pointerdown', holdPiece)
+  grabedTile.containingPiece = 'none'
+  console.log(newTile)
+  newTile.htmlElement.addEventListener('pointerdown', holdPiece)
+  newTile.containingPiece = grabedPiece
+  console.log(newTile.containingPiece)
+
+
+  const tiles = document.getElementsByClassName('cell')
+  for(let i = 0; i < tiles.length;i++){
+    tiles[i].removeEventListener('pointerdown',newPieceTile)
+  }
+  if(playerOrder === 'white'){
+    playerOrder = 'black'
+    playerOpposite = 'white'
+    playerText.textContent = 'Preto'
+    playerText.style.color = 'black'
+  }else if(playerOrder === 'black'){
+    playerOrder = 'white'
+    playerOpposite = 'black'
+    playerText.textContent = 'Branco'
+    playerText.style.color = 'white'
+  }
+  grabing = false
+}
 window.addEventListener('resize', setSizes)
 function setSizes(){
   const cells = document.getElementsByClassName('cell')
-  const hitboxes = document.getElementsByClassName('hitbox')
   const pieces = document.getElementsByClassName('piece')
   cellWidth = getComputedStyle(cells[0]).width
   cellHeight = getComputedStyle(cells[0]).height
 
-  for(let i = 0; i < hitboxes.length; i++){
-    hitboxes[i].style.width = cellWidth
-    hitboxes[i].style.height = cellHeight
-  }
   for(let i = 0; i < pieces.length; i++){
     pieces[i].style.width = (parseFloat(cellWidth)*0.9) + 'px'
   }
@@ -283,15 +364,15 @@ function setSizes(){
 //           piece.style.top = '0px'
 //           piece.style.left = '0px'
 //           piece.style.margin = '0px'
-//     grabedObject = new pieceGrabed(piece)
+//     grabedPiece = new pieceGrabed(piece)
 //     this.style.position = "static"
 //     window.addEventListener('pointermove', grabedMovement)
 //     grabing = true
 //   }
 //   else if(grabing){
 //     console.log('ungrab')
-//     grabedObject.htmlElement.parentElement.style.position = "relative"
-//     grabedObject.htmlElement.parentElement.style.position = "static"
+//     grabedPiece.htmlElement.parentElement.style.position = "relative"
+//     grabedPiece.htmlElement.parentElement.style.position = "static"
 //     grabing = false
 //     window.removeEventListener('pointermove', grabedMovement)
 //   }
@@ -300,7 +381,7 @@ function setSizes(){
 //   if(grabing){
 //     let x = e.clientX
 //     let y = e.clientY
-//     let piece = grabedObject.htmlElement
+//     let piece = grabedPiece.htmlElement
 //     let width = parseFloat(getComputedStyle(piece).width)
 //     let height = parseFloat(getComputedStyle(piece).height)
 //     piece.style.left = (x-width/2) + 'px'
